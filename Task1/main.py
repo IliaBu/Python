0c33e2bdda7dccebea6b7dc4558767770f95211d
IliaBu <bunamaksi@yandex.ru>
2023-03-10 23:38:47 +0500

    Add code task1

---
+++ Task1/main.py
@@ -0,0 +1,13 @@
+# Задача 2: Найдите сумму цифр трёхзначного числа.
+# 123 -> 6 (1 + 2 + 3)
+# 100 -> 1 (1 + 0 + 0)
+
+b = bool(True)
+while b:
+    text = input('Введите трёхзначное число: ')
+    if text.isdigit() == False or len(text) > 3:
+        print('Введено не число! Повторите ввод!')
+    else:
+        b = False
+sum = int(text[0]) + int(text[1]) + int(text[2])
+print(text + ' -> ' + str(sum) + ' (' + text[0] + ' + ' + text[1] + ' + ' + text[2] + ')')
